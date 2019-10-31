6aaf84820684efd778f1e36c86004ba1dcd78ec6
KIM JUN SIK <jinaganda5@gmail.com>
2019-10-31 21:12:09 +0900

    Minitter: Tweet & Follow + JSONEncoder

--- minitter/app.py
+++ minitter/app.py
@@ -1,8 +1,20 @@
 from flask import Flask, jsonify, request
+from flask.json import JSONEncoder
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+
+        return JSONEncoder.default(self, obj)
+
 
 app = Flask(__name__)
 app.users = {}
+app.tweets = []
 app.id_count = 1
+app.json_encoder = CustomJSONEncoder
 
 
 @app.route("/", methods=["GET"])
@@ -17,3 +29,39 @@ def sign_up():
     app.users[app.id_count] = new_user
     app.id_count = app.id_count + 1
     return jsonify(new_user)
+
+
+@app.route("/tweet", methods=["POST"])
+def tweet():
+    payload = request.json
+    user_id = int(payload["id"])
+    tweet = payload["tweet"]
+
+    if user_id not in app.users:
+        return "User not found", 400
+
+    if len(tweet) > 300:
+        return "Too long", 400
+
+    user_id = int(payload["id"])
+    app.tweets.append({
+        "user_id": user_id,
+        "tweet": tweet
+    })
+
+    return "", 200
+
+
+@app.route("/follow", methods=["POST"])
+def follow():
+    payload = request.json
+    user_id = int(payload["id"])
+    user_id_to_follow = int(payload["follow"])
+
+    if user_id not in app.users or user_id_to_follow not in app.users:
+        return "User not found", 400
+
+    user = app.users[user_id]
+    user.setdefault("follow", set()).add(user_id_to_follow)
+
+    return jsonify(user)
